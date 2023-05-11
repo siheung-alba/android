59570feab68573e15e6c939ccc69edfd37f3bea2
jiwon <jiwon0768@tukorea.ac.kr>
2023-05-11 16:55:13 +0900

    유저 마이 페이지 fragment, 이력서 업로드, 급여 계산기, 홈 fragment, 지도 fragment

--- alba/app/src/main/java/com/siheung_alba/alba/activity/ResumeShowActivity.kt
+++ alba/app/src/main/java/com/siheung_alba/alba/activity/ResumeShowActivity.kt
@@ -1,11 +1,12 @@
-package com.siheung_alba.alba
+package com.siheung_alba.alba.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.siheung_alba.alba.R
 
-class MainActivity : AppCompatActivity() {
+class ResumeShowActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_resume_show)
     }
 }
