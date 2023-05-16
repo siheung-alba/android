0014abdb786c8093351c67061bba99dbacde7b54
1-yuna <97020423+1-yuna@users.noreply.github.com>
2023-05-17 01:35:29 +0900

    main + 로그인, 회원가입, 홈

---
+++ alba/app/src/main/java/com/siheung_alba/alba/activity/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.siheung_alba.alba.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.siheung_alba.alba.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
