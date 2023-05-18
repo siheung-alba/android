c529895718778c945a1202423c99a06d624ffee5
Dayeon-Hong <dayon0325@gmail.com>
2023-05-19 00:40:57 +0900

    급여계산기 - 툴바 수정

--- alba/app/src/main/java/com/siheung_alba/alba/fragment/MyPageForUserFragment.kt
+++ alba/app/src/main/java/com/siheung_alba/alba/fragment/MyPageForUserFragment.kt
@@ -6,6 +6,7 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import android.widget.Button
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.ktx.Firebase
 import com.siheung_alba.alba.activity.CalculateSalaryActivity
 import com.siheung_alba.alba.R
 import com.siheung_alba.alba.activity.ResumeUploadActivity
@@ -15,11 +16,14 @@ class MyPageForUserFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(R.layout.fragment_my_page_for_user, container, false)
+        val db = Firebase.alba
         val uploadBtn : Button = view.findViewById(R.id.resume_upload_btn)
         val toolbar: Toolbar = view.findViewById(R.id.myPageToolbar)
 
         toolbar.title = "마이페이지"
-        toolbar.inflateMenu(R.menu.owner_page_menu)
+        toolbar.inflateMenu(R.menu.my_page_menu)
+
+        // 급여계산기 페이지로 이동
         toolbar.setOnMenuItemClickListener { item ->
             when (item.itemId) {
                 R.id.my_page_menu -> {
@@ -32,6 +36,7 @@ class MyPageForUserFragment : Fragment() {
         }
         setHasOptionsMenu(true)
 
+        // 이력서 업로드 페이지로 이동
         uploadBtn.setOnClickListener {
             val intent = Intent(activity, ResumeUploadActivity::class.java)
             startActivity(intent)
