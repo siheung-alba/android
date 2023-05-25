a00565bcd72cfe60ce34bdedfb64a7d5170fd310
jiwon <jiwon0768@tukorea.ac.kr>
2023-05-25 18:07:16 +0900

    홈 화면 공고 리스트 띄우기

--- alba/app/src/main/java/com/siheung_alba/alba/fragment/HomeFragment.kt
+++ alba/app/src/main/java/com/siheung_alba/alba/fragment/HomeFragment.kt
@@ -5,14 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.siheung_alba.alba.R
+import com.siheung_alba.alba.adapter.JobAdapter
+import com.siheung_alba.alba.model.JobModel
 
 
 class HomeFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private val db = Firebase.firestore
+    private val colJobRef = db.collection("job")
+    private val itemList = arrayListOf<JobModel>()
+    private val adapter = JobAdapter(itemList)
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val jobList = view?.findViewById<RecyclerView>(R.id.job_list)
+        jobList?.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        jobList?.adapter = adapter
+
+        colJobRef
+            .get()
+            .addOnSuccessListener { result ->
+                itemList.clear()
+                for(document in result) {
+                    val item = JobModel(
+                        document.data["title"].toString(),
+                        document.data["add_text"].toString(),
+                        document.data["term"].toString(),
+                        document.data["money"].toString(),
+                        document.data["nation"].toString(),
+                        document.data["sex"].toString()
+                    )
+                    itemList.add(item)
+                }
+                adapter.notifyDataSetChanged() // 리사이클러 뷰 갱신
+            }
+            .addOnFailureListener { exception ->
+                android.util.Log.w("MainActivity", "Error getting documents: $exception")
+            }
+
     }
 
 }
