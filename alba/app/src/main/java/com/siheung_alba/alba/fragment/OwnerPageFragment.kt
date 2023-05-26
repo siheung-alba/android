71f27b867eddd5c95e7911c095ce6b672b8d33f8
kang456dh <kang456dh0606@gmail.com>
2023-05-26 15:54:36 +0900

    Auto stash before merge of "donghyun2" and "origin/donghyun2"

--- alba/app/src/main/java/com/siheung_alba/alba/fragment/OwnerPageFragment.kt
+++ alba/app/src/main/java/com/siheung_alba/alba/fragment/OwnerPageFragment.kt
@@ -6,25 +6,73 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import android.widget.Button
 import androidx.appcompat.widget.Toolbar
+<<<<<<< Updated upstream
 import com.siheung_alba.alba.activity.CalculateSalaryActivity
 import com.siheung_alba.alba.R
 import com.siheung_alba.alba.activity.ResumeUploadActivity
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+import com.siheung_alba.alba.R
+import com.siheung_alba.alba.activity.OwnerResume
+import com.siheung_alba.alba.activity.OwnerUpload
+=======
+import com.siheung_alba.alba.activity.CalculateSalaryActivity
+import com.siheung_alba.alba.R
+import com.siheung_alba.alba.activity.ResumeUploadActivity
+>>>>>>> donghyun2
+=======
+import com.siheung_alba.alba.activity.CalculateSalaryActivity
+import com.siheung_alba.alba.R
+import com.siheung_alba.alba.activity.ResumeUploadActivity
+>>>>>>> donghyun2
+>>>>>>> Stashed changes
 
 class OwnerPageFragment : Fragment() {
 
     private var isResumeSubmitted = false //사용자 이력서 제출 여부
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+<<<<<<< Updated upstream
         val view = inflater.inflate(R.layout.fragment_my_page_for_user, container, false)
         val uploadBtn: Button = view.findViewById(R.id.resume_upload_btn)
         val toolbar: Toolbar = view.findViewById(R.id.myPageToolbar)
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+        val view = inflater.inflate(R.layout.owner_mainpage, container, false)
+        val uploadBtn: Button = view.findViewById(R.id.resume_upload_btn)
+        val toolbar: Toolbar = view.findViewById(R.id.ownerPageToolbar)
+=======
+        val view = inflater.inflate(R.layout.fragment_my_page_for_user, container, false)
+        val uploadBtn: Button = view.findViewById(R.id.resume_upload_btn)
+        val toolbar: Toolbar = view.findViewById(R.id.myPageToolbar)
+>>>>>>> donghyun2
+=======
+        val view = inflater.inflate(R.layout.fragment_my_page_for_user, container, false)
+        val uploadBtn: Button = view.findViewById(R.id.resume_upload_btn)
+        val toolbar: Toolbar = view.findViewById(R.id.myPageToolbar)
+>>>>>>> donghyun2
+>>>>>>> Stashed changes
 
         toolbar.title = "채용공고관리"
         toolbar.inflateMenu(R.menu.owner_page_menu)
         toolbar.setOnMenuItemClickListener { item ->
             when (item.itemId) {
                 R.id.owner_page_menu -> {
+<<<<<<< Updated upstream
                     val intent = Intent(activity, CalculateSalaryActivity::class.java)
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+                    val intent = Intent(activity, OwnerResume::class.java)
+=======
+                    val intent = Intent(activity, CalculateSalaryActivity::class.java)
+>>>>>>> donghyun2
+=======
+                    val intent = Intent(activity, CalculateSalaryActivity::class.java)
+>>>>>>> donghyun2
+>>>>>>> Stashed changes
                     startActivity(intent)
                     true
                 }
@@ -34,7 +82,19 @@ class OwnerPageFragment : Fragment() {
         setHasOptionsMenu(true)
 
         uploadBtn.setOnClickListener {
+<<<<<<< Updated upstream
             val intent = Intent(activity, ResumeUploadActivity::class.java)
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+            val intent = Intent(activity, OwnerUpload::class.java)
+=======
+            val intent = Intent(activity, ResumeUploadActivity::class.java)
+>>>>>>> donghyun2
+=======
+            val intent = Intent(activity, ResumeUploadActivity::class.java)
+>>>>>>> donghyun2
+>>>>>>> Stashed changes
             startActivity(intent)
         }
         return view
