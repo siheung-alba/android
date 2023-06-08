168023be93a9187ed03d3ca72d47f9e4fc8e35d8
Dayeon-Hong <dayon0325@gmail.com>
2023-06-09 00:38:41 +0900

    이력서 상세보기 작업중 (리베이스노노)

--- alba/app/src/main/java/com/siheung_alba/alba/model/ResumeModel.kt
+++ alba/app/src/main/java/com/siheung_alba/alba/model/ResumeModel.kt
@@ -1,9 +1,75 @@
 package com.siheung_alba.alba.model
+import android.os.Parcel
+import android.os.Parcelable
+import java.util.*
 
-class ResumeModel (
-    var email: String?,
-    var title: String?,
-    var career: String?,
-    var introduce: String?,
-    var updated_at: String?
-)
+class ResumeModel(
+    val email: String?,
+    val title: String?,
+    val career: String?,
+    val created_at : String?,
+    val resume_id : String?,
+    val introduce: String?,
+    val updated_at: String?
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    )
+
+    constructor(
+        email: String?,
+        title: String?,
+        career: String?,
+        introduce: String?,
+        updated_at: String?
+    ) : this(
+        email,
+        title,
+        career,
+        getCurrentDate(),
+        generateResumeId(),
+        introduce,
+        updated_at
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(introduce)
+        parcel.writeString(updated_at)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ResumeModel> {
+        override fun createFromParcel(parcel: Parcel): ResumeModel {
+            return ResumeModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ResumeModel?> {
+            return arrayOfNulls(size)
+        }
+
+        private fun getCurrentDate(): String {
+            val calendar = Calendar.getInstance()
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+            val month = calendar.get(Calendar.MONTH) + 1
+            val year = calendar.get(Calendar.YEAR)
+            return "$month/$day/$year"
+        }
+
+        private fun generateResumeId(): String {
+            // 랜덤한 resume_id 생성 로직을 구현해야 함
+            // 예시로 UUID를 사용하여 랜덤한 문자열 생성
+            return UUID.randomUUID().toString()
+        }
+    }
+}
--- alba/app/src/main/java/com/siheung_alba/alba/user/ResumeShowActivity.kt
+++ alba/app/src/main/java/com/siheung_alba/alba/user/ResumeShowActivity.kt
@@ -2,13 +2,35 @@ package com.siheung_alba.alba.user
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.siheung_alba.alba.R
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
+// 이력서 보여주기 페이지 (수정, 삭제)
 class ResumeShowActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val db = Firebase.firestore
+
+    private val current = LocalDateTime.now()
+    private val formatter = DateTimeFormatter.ofPattern("M/d")
+    private val formatted = current.format(formatter)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resume_show)
+
+        val toolbar: Toolbar = findViewById(R.id.resumeShowToolbar)
+        toolbar.title = "상세보기"
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayShowTitleEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        auth = FirebaseAuth.getInstance()
 
         // 이력서 정보 가져와서 화면에 표시하는 로직 구현
         val resumeId = intent.getStringExtra("resumeId")
