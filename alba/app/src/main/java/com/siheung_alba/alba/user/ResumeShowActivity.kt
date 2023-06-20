248b6e003e8fdd02bebbf8d15a90d73b0db906a7
Dayeon-Hong <dayon0325@gmail.com>
2023-06-20 15:25:32 +0900

    리사이클러뷰 오류

--- alba/app/src/main/java/com/siheung_alba/alba/user/ResumeShowActivity.kt
+++ alba/app/src/main/java/com/siheung_alba/alba/user/ResumeShowActivity.kt
@@ -1,6 +1,13 @@
 package com.siheung_alba.alba.user
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.google.firebase.auth.FirebaseAuth
@@ -15,6 +22,8 @@ class ResumeShowActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private val db = Firebase.firestore
+    private val colResumeRef = db.collection("resume")
+
 
     private val current = LocalDateTime.now()
     private val formatter = DateTimeFormatter.ofPattern("M/d")
@@ -31,14 +40,72 @@ class ResumeShowActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+        val intent = intent
 
-        // 이력서 정보 가져와서 화면에 표시하는 로직 구현
-        val resumeId = intent.getStringExtra("resumeId")
+        val changeBtn: Button = findViewById(R.id.change_resume)
+
+        // 이력서 정보를 받아오기 위해 인텐트에서 데이터 추출
+        val resumeId = intent.getStringExtra("resume_id")
+        val resumeTitle = intent.getStringExtra("title")
+        val resumeCareer = intent.getStringExtra("career")
+        val resumeIntroduce = intent.getStringExtra("introduce")
+        val receivedUserName = intent.getStringExtra("userName")
+        val receivedUserSex = intent.getStringExtra("userSex")
+        val receivedUserAge = intent.getStringExtra("userAge")
+        val receivedUserNation = intent.getStringExtra("userNation")
+
+        // 이력서 정보를 화면에 표시
+        val titleEditText: EditText = findViewById(R.id.resume_show_title)
+        val careerEditText: EditText = findViewById(R.id.resume_show_career)
+        val introEditText: EditText = findViewById(R.id.resume_show_introduce)
+        val userNameTextView : TextView = findViewById(R.id.resume_show_text1) // 이름
+        val userSexTextView : TextView = findViewById(R.id.resume_show_text2) // 성별
+        val userAgeTextView : TextView = findViewById(R.id.resume_show_text3) // 나이
+        val userNationTextView : TextView = findViewById(R.id.resume_show_text4) // 국적
+
+        titleEditText.setText(resumeTitle)
+        careerEditText.setText(resumeCareer)
+        introEditText.setText(resumeIntroduce)
+        userNameTextView.text = receivedUserName
+        userSexTextView.text = receivedUserSex
+        userAgeTextView.text = receivedUserAge
+        userNationTextView.text = receivedUserNation
+
 
         // TODO: 이력서 정보를 사용하여 화면에 표시하는 로직 작성
 
-        // 수정 버튼 클릭 시 이력서 수정 페이지로 이동
-        // TODO: 수정 버튼 클릭 시 이력서 수정 페이지로 이동하는 코드 작성
+        changeBtn.setOnClickListener {
+            // 수정된 내용을 저장
+            val editedTitle = titleEditText.text.toString()
+            val editedCareer = careerEditText.text.toString()
+            val editedIntroduce = introEditText.text.toString()
+
+            val resumeData = hashMapOf<String, Any>(
+                "title" to editedTitle,
+                "career" to editedCareer,
+                "introduce" to editedIntroduce
+            )
+
+            db.collection("resume")
+                .document(resumeId!!)
+                .update(resumeData)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "이력서가 성공적으로 업데이트되었습니다", Toast.LENGTH_SHORT).show()
+                    val resultIntent = Intent()
+                    resultIntent.putExtra("editedTitle", editedTitle)
+                    resultIntent.putExtra("editedCareer", editedCareer)
+                    resultIntent.putExtra("editedIntroduce", editedIntroduce)
+                    setResult(Activity.RESULT_OK, resultIntent)
+                    finish()
+                }
+                .addOnFailureListener { exception ->
+                    Log.e("ResumeShowActivity", "Error updating resume: $exception")
+                    Toast.makeText(this, "이력서 업데이트에 실패했습니다", Toast.LENGTH_SHORT).show()
+                }
+
+        }
+
 
         // 삭제 버튼 클릭 시 이력서 삭제 로직 구현
         // TODO: 삭제 버튼 클릭 시 이력서 삭제 로직 작성
